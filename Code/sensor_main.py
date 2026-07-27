b2dbd4dc1982b0fd5b098d2786dcf228dbf8c0af
PhantomCNS2 <dangerman01013@gmail.com>
2026-07-27 04:06:32 +0300

    Added initializations

--- Code/sensor_main.py
+++ Code/sensor_main.py
@@ -3,3 +3,19 @@ import time
 import dht
 import network
 import uart
+
+# ================================
+# variables
+dht_Pin = 4  # DHT sensor pin
+gas_Pin = 34  # Gas sensor pin
+# ================================
+
+# ================================
+# Initialize DHT sensor
+dht_Sensor = dht.DHT22(Pin(dht_Pin))
+
+# ================================
+# Initialize Gas sensor
+gas_Sensor = ADC(Pin(gas_Pin))
+gas_Sensor.atten(ADC.ATTN_11DB)  # Set attenuation level
+# ================================
